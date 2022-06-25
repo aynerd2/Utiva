8f4510cd87fe0cbdb3a614abcd6bf6109ad09947
aynerd <olawaleayowande@gmail.com>
2022-06-25 13:02:40 +0100

    Firsts Commit

---
+++ utiva/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>UTIVA PROJECT</h1>
+      <img src="logo192.png" className="logo"/>
+    </div>
+  );
+}
+
+export default App;
